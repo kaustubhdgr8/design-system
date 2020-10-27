31c66a3aa593320072938087fa294c9a489a88bc
Kaustubh Ghodke <kaustubhdgr8@gmail.com>
2020-10-27 11:36:20 +0530

    initial code setup

---
+++ Gruntfile.js
@@ -0,0 +1,127 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: ['public/**/*'],
+    concat: {
+      js: {
+        src: ['src/js/**/*.js', '!src/js/main.js', '!src/js/main.min.js'],
+        dest: 'src/js/main.js'
+      }
+    },
+    copy: {
+      js: {
+        expand: true,
+        flatten: true,
+        src: ['src/js/main.min.js'],
+        dest: 'public/'
+      },
+      html: {
+        expand: true,
+        flatten: true,
+        src: ['src/templates/index.html'],
+        dest: 'public/'
+      },
+      css: {
+        expand: true,
+        flatten: true,
+        src: ['src/css/styles.min.css'],
+        dest: 'public/'
+      },
+      assets: {
+        expand: true,
+        flatten: false,
+        src: ['assets/**/*'],
+        dest: 'public/'
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'src/css/styles.min.css': ['src/css/*.css', '!src/css/*.min.css']
+        }
+      }
+    },
+    handlebars: {
+      compile: {
+        files: {
+          'src/js/templates.js': ['src/templates/**/*.hbs']
+        }
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'src/templates/index.html': 'src/templates/template.html'
+        }
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'src/js/**/*.js', '!src/js/main.min.js', '!src/js/templates.js'],
+      options: {
+        'esversion': 6
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/css',
+          src: ['styles.scss'],
+          dest: '.',
+          ext: '.css'
+        }]
+      }
+    },
+    uglify: {
+      js: {
+        files: {
+          'src/js/main.min.js': ['src/js/main.js']
+        }
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: ['jshint']
+      },
+      js: {
+        files: ['src/js/**/*', '!src/js/**/*.min.js'],
+        tasks: ['compile--js', 'build']
+      },
+      handlebar: {
+        files: ['src/**/*'],
+        tasks: ['compile--js', 'build']
+      },
+      css: {
+        files: ['src/**/*'],
+        tasks: ['compile--css', 'build']
+      },
+      html: {
+        files: ['src/**/*'],
+        tasks: ['compile--html', 'build']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['compile', 'build', 'watch']);
+  grunt.registerTask('compile', ['compile--js', 'compile--css', 'compile--html']);
+  grunt.registerTask('compile--js', ['jshint', 'handlebars', 'concat', 'uglify']);
+  grunt.registerTask('compile--css', ['sass', 'cssmin', 'htmlmin']);
+  grunt.registerTask('compile--html', ['htmlmin']);
+  grunt.registerTask('build', ['clean', 'copy']);
+};
